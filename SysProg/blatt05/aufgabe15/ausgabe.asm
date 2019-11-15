7a48f26140794fd68bc2907e34465afb3a63724d
Tobias Ctistis <tctistis-cs@bitbucket.com>
2019-11-15 16:13:55 +0100

    SysProg Ü5 Nr 15 FINAL

--- SysProg/blatt05/aufgabe15/ausgabe.asm
+++ SysProg/blatt05/aufgabe15/ausgabe.asm
@@ -1,27 +1,27 @@
 ; Definition der externen glibc-Funktionen
 global main
 
+extern puts
+extern fgets
+extern stdin
 
+SECTION .bss
+buff: resw 21
 
 ; Daten-Segment           
 SECTION .data
 
 ;Buffer für 20 ASCII-Zeichen
-buff: db `12345678901234567890`, 0
-bufflen: equ $ - buff
+;buff: db `12345678901234567890`, 0
+bufflen: db 21
 
 ; Text-Segment (Code)
 SECTION .text
-
-extern puts
-extern fgets
-extern stdin
-
 main:		; Parameter für fgets übergeben
 		; Signatur: char *fgets(char *str, int n, FILE *stream)
 		mov rdi, buff
 		mov rsi, bufflen
-		mov rdx, stdin
+		mov rdx, [stdin]
 									; Filedeskriptor 0 ist stdin
 		; Einlesen von stdin per glibc-Aufruf
 		call fgets
@@ -30,40 +30,42 @@ main:		; Parameter für fgets übergeben
 		; Prüfung ob Returnwert NULL	
 		add rax, 0						
 		jz exit
-
+			
 		; String-Manipulation:
 		; ToUpper + Kein Zeilenumbruch	+ Nullterminieren!
 		
 		; Über String laufen
 		mov rcx, 0
-		
+		;mov byte [buff+rcx], 'x'
 schleife:	mov r8, [buff+rcx]				; Char laden
-		jz weiter
+		add r8, 0
+		jz ausgabe
 		
 		; Line Feed/Neue Zeile: 10
 
 		cmp r8, 10
-		jne weiter1
-		mov r9, buff
+		jne weiter
+		
 		mov byte [buff+rcx], 32				; Zeilenumbruch durch Leerzeichen ersetzen
+		jmp incCounter
 
-weiter1:	; Kleinbuchstaben: a (97) - 122 (z)
-		cmp r8, 97
-		jb incCounter
+weiter:		; Kleinbuchstaben: a (97) - 122 (z)
+	
+		cmp r8b, 'a'
+		jl incCounter
 
-		cmp r8, 122
-		ja incCounter
+		cmp r8b, 'z'
+		jg incCounter
 
 		sub r8, 32					; ToUpperCase()
 		mov [buff+rcx], r8
 		
-incCounter:	add rcx, 1
+incCounter:	inc rcx
 		jmp schleife							
 								; Sprungbedingung		
-weiter:		; Parameter für puts übergeben
+ausgabe:	; Parameter für puts übergeben
 		; Signatur: int puts(const char *str)
 		mov rdi, buff
-		
 		call puts					; Ausgabe in stdout per glibc-Aufruf
 
 exit:		; ... Programm mit ErrorCode 0 beenden.
